9034078478f37f2b1a4152630cf56d6c10a469b0
shafiradwi20 <148737458+shafiradwi20@users.noreply.github.com>
2023-10-27 18:59:16 +0700

    Add files via upload

---
+++ SI_RegPagi_22166018_latihan18/src/si_regpagi_22166018_latihan18/SI_RegPagi_22166018_latihan18.java
@@ -0,0 +1,22 @@
+/*
+Nama      : Shafira Dwi Nur'Izzati
+Nim       : 22166018
+Prodi     : Sistem Informasi
+Deskripsi : Program Kelipatan
+*/
+package si_regpagi_22166018_latihan18;
+public class SI_RegPagi_22166018_latihan18 {
+    public static void main(String[] args) {
+        System.out.println("Bilangan kelipatan 3.5 dari 3.5 hingga 35:");
+
+        for (double i = 3.5; i <= 35; i+= 0.5) {
+            // Cek apakah bilangan merupakan kelipatan 3.5 
+            if (i % 3.5 == 0) {
+                System.out.println(i);
+    }
+        }
+    }
+}
+
+    
+
